1ba146b9e85656d7cd172b27571e83e4a3f4e6c5
Ray <rlawlsdn9583@gmail.com>
2024-11-19 17:14:25 +0900

    chore: 뒤로가기 버튼 및 저장 버튼 UI 추가

--- app/board/[id]/page.tsx
+++ app/board/[id]/page.tsx
@@ -1,14 +1,12 @@
 "use client";
 
-import { useState } from "react";
-import { format } from "date-fns";
-import { cn } from "@/lib/utils";
-import { Button, Calendar, SearchBar, Progress, Popover, PopoverContent, PopoverTrigger } from "@/components/ui";
-import { Calendar as CalendarIcon } from "lucide-react";
+import Image from "next/image";
+import { Button, Card, SearchBar, Progress, LabelDatePicker, Separator, Checkbox } from "@/components/ui";
+import { ChevronLeft, ChevronUp } from "lucide-react";
 import styles from "./page.module.scss";
 
 function BoardPage() {
-  const [date, setDate] = useState<Date>();
+  // const createBoard = () => {};
 
   return (
     <div className="page">
@@ -36,65 +34,89 @@ function BoardPage() {
       </aside>
       <main className="page__main">
         <div className={styles.header}>
+          <div className={styles[`header__btn-box`]}>
+            <Button variant={"outline"} size={"icon"}>
+              <ChevronLeft />
+            </Button>
+            <Button variant={"secondary"}>저장</Button>
+          </div>
           <div className={styles.header__top}>
             {/* 제목 입력 Input 섹션 */}
             <input type="text" placeholder="Enter Title Here!" className={styles.header__top__input} />
             {/* 진행상황 척도 그래프 섹션 */}
             <div className="flex items-center justify-start gap-4">
               <small className="text-sm font-medium leading-none text-[#6D6D6D]">1/10 Completed</small>
-              <Progress className="w-60 h-[10px]" />
+              <Progress className="w-60 h-[10px]" value={33} />
             </div>
           </div>
           {/* 캘린더 + Add New Board 버튼 섹션 */}
           <div className={styles.header__bottom}>
-            <div className="flex items-center">
-              <div className="flex items-center gap-3">
-                <small className="text-sm font-medium leading-none text-[#6D6D6D]">From </small>
-                <Popover>
-                  <PopoverTrigger asChild>
-                    <Button
-                      variant={"outline"}
-                      className={cn(
-                        "w-[280px] justify-start text-left font-normal",
-                        !date && "text-muted-foreground"
-                      )}
-                    >
-                      <CalendarIcon className="mr-2 h-4 w-4" />
-                      {date ? format(date, "PPP") : <span>Pick a date</span>}
-                    </Button>
-                  </PopoverTrigger>
-                  <PopoverContent className="w-auto p-0">
-                    <Calendar mode="single" selected={date} onSelect={setDate} initialFocus />
-                  </PopoverContent>
-                </Popover>
-              </div>
-              <div className="flex items-center gap-3">
-                <small className="text-sm font-medium leading-none text-[#6D6D6D]">To</small>
-                <Popover>
-                  <PopoverTrigger asChild>
-                    <Button
-                      variant={"outline"}
-                      className={cn(
-                        "w-[280px] justify-start text-left font-normal",
-                        !date && "text-muted-foreground"
-                      )}
-                    >
-                      <CalendarIcon className="mr-2 h-4 w-4" />
-                      {date ? format(date, "PPP") : <span>Pick a date</span>}
-                    </Button>
-                  </PopoverTrigger>
-                  <PopoverContent className="w-auto p-0">
-                    <Calendar mode="single" selected={date} onSelect={setDate} initialFocus />
-                  </PopoverContent>
-                </Popover>
-              </div>
+            <div className="flex items-center gap-5">
+              <LabelDatePicker label={"From"} />
+              <LabelDatePicker label={"To"} />
             </div>
-            <Button className="text-white bg-[#E79057] hover:bg-[#E79057] hover:border hover:border-[#E26F24]">
+            <Button className="text-white bg-[#E79057] hover:bg-[#E26F24] hover:ring-1 hover:ring-[#E26F24] hover:ring-offset-1 active:bg-[#D5753D] hover:shadow-lg">
               Add New Board
             </Button>
           </div>
         </div>
-        <div className={styles.body}></div>
+        <div className={styles.body}>
+          {/* Add New Board 버튼 클릭으로 인한 Board 데이터가 없을 경우 */}
+          {/* <div className={styles.body__noData}>
+                        <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">There is no board yet.</h3>
+                        <small className="text-sm font-medium leading-none text-[#6D6D6D] mt-3 mb-7">
+                            Click the button and start flashing!
+                        </small>
+                        <button onClick={createBoard}>
+                            <Image src="/assets/images/button.svg" width={74} height={74} alt="rounded-button" />
+                        </button>
+                    </div> */}
+          {/* Add New Board 버튼 클릭으로 인한 Board 데이터가 있을 경우 */}
+          <div className={styles.body__isData}>
+            <Card className="w-full flex flex-col items-center p-5">
+              {/* 게시물 카드 제목 영역*/}
+              <div className="w-full flex items-center justify-between mb-4">
+                <div className="flex items-center justify-start gap-2">
+                  <Checkbox className="h-5 w-5" />
+                  <input
+                    type="text"
+                    placeholder="제목을 입력하세요."
+                    className="text-xl outline-none"
+                    disabled={true}
+                  />
+                </div>
+                <Button variant={"ghost"} size={"icon"}>
+                  <ChevronUp className="text-[#6d6d6d]" />
+                </Button>
+              </div>
+              {/* 캘린더 및 버튼 박스 영역 */}
+              <div className="w-full flex items-center justify-between">
+                {/* 캘린더 박스 */}
+                <div className="flex items-center gap-5">
+                  <LabelDatePicker label={"From"} />
+                  <LabelDatePicker label={"To"} />
+                </div>
+                {/* 버튼 박스 */}
+                <div className="flex items-center">
+                  <Button variant={"ghost"} className="font-normal text-[#6D6D6D]">
+                    Duplicate
+                  </Button>
+                  <Button
+                    variant={"ghost"}
+                    className="font-normal text-rose-600 hover:text-rose-600 hover:bg-red-50"
+                  >
+                    Delete
+                  </Button>
+                </div>
+              </div>
+              <Separator className="my-3" />
+              {/* Add Contents 버튼 영역 */}
+              <Button variant={"ghost"} className="font-normal text-[#6D6D6D]">
+                Add Contents
+              </Button>
+            </Card>
+          </div>
+        </div>
       </main>
     </div>
   );
