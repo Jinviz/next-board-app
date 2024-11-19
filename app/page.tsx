6b239f8bd697086843444757d2648d41f72a6b77
Ray <rlawlsdn9583@gmail.com>
2024-11-19 17:25:11 +0900

    fix: page 에러 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,21 @@
-import { Button, SearchBar, Progress, LabelDatePicker } from "@/components/ui";
-import styles from "./page.module.scss";
+"use client";
 
-function BoardPage() {
+import { useRouter } from "next/navigation";
+import { Button, SearchBar } from "@/components/ui";
+
+function InitPage() {
+  const router = useRouter();
+
   return (
     <div className="page">
       <aside className="page__aside">
         {/* 검색창 UI */}
         <SearchBar placeholder="검색어를 입력하세요." />
         {/* Add New Page 버튼 UI */}
-        <Button className="text-[#E79057] bg-white border border-[#E79057] hover:bg-[#FFF9F5]">
+        <Button
+          className="text-[#E79057] bg-white border border-[#E79057] hover:bg-[#FFF9F5]"
+          onClick={() => router.push("/board/1")}
+        >
           Add New Page
         </Button>
         {/* TODO 목록 UI 하나 */}
@@ -27,31 +34,22 @@ function BoardPage() {
         </div>
       </aside>
       <main className="page__main">
-        <div className={styles.header}>
-          <div className={styles.header__top}>
-            {/* 제목 입력 Input 섹션 */}
-            <input type="text" placeholder="Enter Title Here!" className={styles.header__top__input} />
-            {/* 진행상황 척도 그래프 섹션 */}
-            <div className="flex items-center justify-start gap-4">
-              <small className="text-sm font-medium leading-none text-[#6D6D6D]">1/10 Completed</small>
-              <Progress className="w-60 h-[10px]" />
-            </div>
-          </div>
-          {/* 캘린더 + Add New Board 버튼 섹션 */}
-          <div className={styles.header__bottom}>
-            <div className="flex items-center gap-5">
-              <LabelDatePicker label={"From"} />
-              <LabelDatePicker label={"To"} />
-            </div>
-            <Button className="text-white bg-[#E79057] hover:bg-[#E79057] hover:border hover:border-[#E26F24]">
-              Add New Board
-            </Button>
+        <div className="flex flex-col items-center justify-center gap-5 mb-6">
+          <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">How to start:</h3>
+          <div className="flex flex-col items-center gap-3">
+            <small className="text-sm font-normal leading-none">1. Create a page</small>
+            <small className="text-sm font-normal leading-none">2. Add boards to page</small>
           </div>
         </div>
-        <div className={styles.body}></div>
+        <Button
+          className="text-[#E79057] bg-transparent border border-[#E79057] hover:bg-[#FFF9F5] w-[180px]"
+          onClick={() => router.push("/board/1")}
+        >
+          Add New Page
+        </Button>
       </main>
     </div>
   );
 }
 
-export default BoardPage;
+export default InitPage;
